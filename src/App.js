a067d597043e74c1fcbfa6b16fd068492a0a72a9
Vania Sameen <128991354+vaniaSam@users.noreply.github.com>
2024-11-09 22:40:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-// App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
-import Home from './Home';  // Import Home Page
-import DonateFood from './DonateFood';  // Import Donate Food Page
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { DonorPage } from './DonorPage';
+import { ReceiverPage } from './User';
+import { EligibilityForm } from './EligibilityForm';
 
 function App() {
+  const [userRole, setUserRole] = useState(null); // Track user role (donor or receiver)
+
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/donate" element={<DonateFood />} />
-        </Routes>
+      <div>
+        <h1>Food Donation App</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/donor" component={DonorPage} />
+          <Route path="/receiver" component={ReceiverPage} />
+          <Route path="/eligibility" component={EligibilityForm} />
+        </Switch>
       </div>
     </Router>
   );
